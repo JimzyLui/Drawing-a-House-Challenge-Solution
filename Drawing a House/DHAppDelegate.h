84f70c4e062c7012449487531802cc5593e23dfd
Jimzy Lui <jimzylui_desktop@stickyaznrabbit.com>
2013-11-23 00:41:25 -0500

    Initial Commit

---
+++ Drawing a House/DHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DHAppDelegate.h
+//  Drawing a House
+//
+//  Created by Jimzy Lui on 11/23/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Drawing a House/DHViewController.h
@@ -0,0 +1,13 @@
+//
+//  DHViewController.h
+//  Drawing a House
+//
+//  Created by Jimzy Lui on 11/23/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHViewController : UIViewController
+
+@end
